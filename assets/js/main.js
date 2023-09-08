503dea387b323fa85dda9492d78bab3c38746757
Mayur Dhagiya <mayurdhagiya@gmail.com>
2023-09-08 16:02:33 +0530

    new template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,69 +1,115 @@
+/*
+Template Name: Appvilla - Creative Landing Page HTML Template.
+Author: GrayGrids
+*/
+
+(function () {
+    //===== Prealoder
+
+    window.onload = function () {
+        window.setTimeout(fadeout, 500);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
 
 
+    /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.querySelector(".navbar-area");
+        var sticky = header_navbar.offsetTop;
 
-$(function () {
-"use strict";
+        var logo = document.querySelector('.navbar-brand img')
+        if (window.pageYOffset > sticky) {
+          header_navbar.classList.add("sticky");
+          logo.src = 'assets/images/logo/logo.svg';
+        } else {
+          header_navbar.classList.remove("sticky");
+          logo.src = 'assets/images/logo/white-logo.svg';
+        }
+
+        // show or hide the back-top-top button
+        var backToTo = document.querySelector(".scroll-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
 
 
-	// data - background
-	$("[data-background]").each(function () {
-		$(this).css("background-image", "url(" + $(this).attr("data-background") + ")");
-	});
+    
+    // section menu active
+	function onScroll(event) {
+		var sections = document.querySelectorAll('.page-scroll');
+		var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 
-	// contact-form 
-	$(".contact-btn").on('click', function () {
-		$(".contact-wrapper").addClass("show");
-	});
-	$(".close").on('click', function () {
-		$(".contact-wrapper").removeClass("show");
-	});
-
-	// contact-form-28
-	$("#contact-btn").on('click', function () {
-		$("#contact-wrapper").addClass("show");
-	});
-	$(".close").on('click', function () {
-		$("#contact-wrapper").removeClass("show");
-	});
-	// subscribe-form-28
-	$("#subscribe-btn").on('click', function () {
-		$("#subscribe-wrapper").addClass("show");
-	});
-	$(".close").on('click', function () {
-		$("#subscribe-wrapper").removeClass("show");
-	});
-
-	// sidebar
-	$(".menu-toggle-btn").on('click', function () {
-		$(".sidebar").addClass("show");
-	});
-	$(".close").on('click', function () {
-		$(".sidebar").removeClass("show");
-	});
-
-	//Scroll top 
-	$(".scroll-top").click(function () {
-		$("html,body").animate({ scrollTop: 0 }, 1000);
-	});
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 180) { $(".scroll-top").fadeIn(); }
-		else {
-			$(".scroll-top").fadeOut();
+		for (var i = 0; i < sections.length; i++) {
+			var currLink = sections[i];
+			var val = currLink.getAttribute('href');
+			var refElement = document.querySelector(val);
+			var scrollTopMinus = scrollPos + 73;
+			if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+				document.querySelector('.page-scroll').classList.remove('active');
+				currLink.classList.add('active');
+			} else {
+				currLink.classList.remove('active');
+			}
 		}
-	});
+	};
 
-	//Countdown
+    window.document.addEventListener('scroll', onScroll);
+    
+    // for menu scroll 
+    var pageLink = document.querySelectorAll('.page-scroll');
 
-    $('[data-countdown]').each(function () {
-		var $this = $(this),
-			finalDate = $(this).data('countdown');
-		$this.countdown(finalDate, function (event) {
-			$this.html(event.strftime('<div class="countdown d-flex"><div class="single-count-content"><span class="count">%D</span><p class="text">Days</p></div><div class="single-count-content"><span class="count">%H</span><p class="text">Hours</p></div><div class="single-count-content"><span class="count">%M</span><p class="text">Minutes</p></div><div class="single-count-content"><span class="count">%S</span><p class="text">Seconds</p></div></div>'));
-		});
-	});
+    pageLink.forEach(elem => {
+        elem.addEventListener('click', e => {
+            e.preventDefault();
+            document.querySelector(elem.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                offsetTop: 1 - 60,
+            });
+        });
+    });
 
-	// WOW active
-	new WOW().init();
+    // WOW active
+    new WOW().init();
 
-});	
+    let filterButtons = document.querySelectorAll('.portfolio-btn-wrapper button');
+    filterButtons.forEach(e =>
+        e.addEventListener('click', () => {
 
+            let filterValue = event.target.getAttribute('data-filter');
+            iso.arrange({
+                filter: filterValue
+            });
+        })
+    );
+
+    var elements = document.getElementsByClassName("portfolio-btn");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].onclick = function () {
+            var el = elements[0];
+            while (el) {
+                if (el.tagName === "BUTTON") {
+                    el.classList.remove("active");
+                }
+                el = el.nextSibling;
+            }
+            this.classList.add("active");
+        };
+    };
+
+    //===== mobile-menu-btn
+    let navbarToggler = document.querySelector(".mobile-menu-btn");
+    navbarToggler.addEventListener('click', function () {
+        navbarToggler.classList.toggle("active");
+    });
+
+
+})();
